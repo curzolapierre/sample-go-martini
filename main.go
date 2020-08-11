e04db981ae5cd5197b92b5df9b5be864bf31e1ec
CURZOLA Pierre <pierre.curzola@gmail.com>
2020-08-11 11:54:46 +0200

    test crash

--- main.go
+++ main.go
@@ -66,6 +66,10 @@ func main() {
 		panic(err)
 	}
 
+	fmt.Fprintf(os.Stderr, "CRASHING !!! (stderr)\n")
+	fmt.Fprintf(os.Stdout, "CRASHING !!! (stdout)\n")
+	os.Exit(1)
+
 	go http.Serve(listener, m)
 	log.Println("Listening on 0.0.0.0:" + port)
 
