94f6d438d010d7ed68a3dae0d9199106c68398b9
CURZOLA Pierre <pierre.curzola@gmail.com>
2021-01-18 09:44:38 +0100

    update go and deps

--- vendor/github.com/oxtoacart/bpool/bytepool.go
+++ vendor/github.com/oxtoacart/bpool/bytepool.go
@@ -1,18 +1,15 @@
 package bpool
 
-/*
-BytePool implements a leaky pool of []byte in the form of a bounded
-channel.
-*/
+// BytePool implements a leaky pool of []byte in the form of a bounded
+// channel.
 type BytePool struct {
 	c chan []byte
 	w int
+	h int
 }
 
-/*
-NewBytePool creates a new BytePool bounded to the given maxSize, with new byte
-arrays sized based on width.
-*/
+// NewBytePool creates a new BytePool bounded to the given maxSize, with new
+// byte arrays sized based on width.
 func NewBytePool(maxSize int, width int) (bp *BytePool) {
 	return &BytePool{
 		c: make(chan []byte, maxSize),
@@ -20,10 +17,8 @@ func NewBytePool(maxSize int, width int) (bp *BytePool) {
 	}
 }
 
-/*
-Get gets a []byte from the BytePool, or creates a new one if none are available
-in the pool.
-*/
+// Get gets a []byte from the BytePool, or creates a new one if none are
+// available in the pool.
 func (bp *BytePool) Get() (b []byte) {
 	select {
 	case b = <-bp.c:
@@ -35,21 +30,27 @@ func (bp *BytePool) Get() (b []byte) {
 	return
 }
 
-/*
-Put returns the given Buffer to the BytePool.
-*/
+// Put returns the given Buffer to the BytePool.
 func (bp *BytePool) Put(b []byte) {
+	if cap(b) < bp.w {
+		// someone tried to put back a too small buffer, discard it
+		return
+	}
+
 	select {
-	case bp.c <- b:
+	case bp.c <- b[:bp.w]:
 		// buffer went back into pool
 	default:
 		// buffer didn't go back into pool, just discard
 	}
 }
 
-/*
-Width returns the width of the byte arrays in this pool.
-*/
+// NumPooled returns the number of items currently pooled.
+func (bp *BytePool) NumPooled() int {
+	return len(bp.c)
+}
+
+// Width returns the width of the byte arrays in this pool.
 func (bp *BytePool) Width() (n int) {
 	return bp.w
 }
