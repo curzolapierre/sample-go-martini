77d472673e6e7d11cd18be59242322bc5765c85c
CURZOLA Pierre <pierre.curzola@gmail.com>
2021-02-24 18:17:24 +0100

    working commit: GRC

--- main.go
+++ main.go
@@ -66,9 +66,11 @@ func main() {
 		panic(err)
 	}
 
-	fmt.Fprintf(os.Stderr, "CRASHING !!! (stderr)\n")
-	fmt.Fprintf(os.Stdout, "CRASHING !!! (stdout)\n")
-	os.Exit(1)
+	if os.Getenv("CRASHING") != "" {
+		fmt.Fprintf(os.Stderr, "CRASHING !!! (stderr)\n")
+		fmt.Fprintf(os.Stdout, "CRASHING !!! (stdout)\n")
+		os.Exit(1)
+	}
 
 	go http.Serve(listener, m)
 	log.Println("Listening on 0.0.0.0:" + port)
